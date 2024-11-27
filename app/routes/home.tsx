7b5017466eea6639614b7a50fb2cd140b2ff2662
kritikdevx <kritik.jiyaviya@devxconsultancy.com>
2024-11-27 22:26:34 +0530

    setup shadcn and prettier

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <h1>Welcome to React Router!</h1>
+      <p>
+        This is a new React Router app. You can start by editing{' '}
+        <code>app/routes/home.tsx</code>.
+      </p>
+    </div>
+  );
 }
